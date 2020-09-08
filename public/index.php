f649d2a236fbd90b7369fc41ff2825f670673102
trevk <trev@candidsoftware.com>
2020-09-08 17:04:38 +0100

    various sass changes

--- public/index.php
+++ public/index.php
@@ -4,14 +4,23 @@ require_once '../vendor/autoload.php';
 //Load Twig templating environment
 $loader = new Twig_Loader_Filesystem('../templates/');
 $twig = new Twig_Environment($loader, ['debug' => true]);
-
-//Get the episodes from the API
-$client = new GuzzleHttp\Client();
-$res = $client->request('GET', 'http://3ev.org/dev-test-api/');
-$data = json_decode($res->getBody(), true);
-
-//Sort the episodes
-array_multisort(array_keys($data), SORT_ASC, SORT_STRING, $data);
+$data = [];
+$error = null;
+try {
+	//Get the episodes from the API
+	$client = new GuzzleHttp\Client();
+	$res = $client->request('GET', 'http://3ev.org/dev-test-api/');
+	$data = json_decode($res->getBody(), true);
+	//Sort the episodes
+	usort($data, function ($a, $b) {
+		if ($a['season'] == $b['season']) {
+			return $a['episode'] - $b['episode'];
+		}
+		return $a['season'] - $b['season'];
+	});
+} catch (Exception $e) {
+	$error = 'Sorry, there was an error. Please try again by reloading the page.';
+}
 
 //Render the template
-echo $twig->render('page.html', ["episodes" => $data]);
+echo $twig->render('page.html', ["episodes" => $data, "error" => $error]);
